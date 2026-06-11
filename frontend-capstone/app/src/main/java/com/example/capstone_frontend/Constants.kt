72649699de7599a8505d992dbfcf2dcdab9eb455
Fariz Ubaidillah <frizbae04@gmail.com>
2026-06-11 19:30:39 +0700

    Perbaikan docker-compose dan TTL status transaksi

--- frontend-capstone/app/src/main/java/com/example/capstone_frontend/Constants.kt
+++ frontend-capstone/app/src/main/java/com/example/capstone_frontend/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
     // CHANGE THIS STRING MANUALLY BASED ON YOUR DEVICE:
     // "10.0.2.2:8080"      -> If testing on Android Studio Emulator
 
-    const val SERVER_IP_AND_PORT = "192.168.100.194:8080"
+    const val SERVER_IP_AND_PORT = "192.168.123.100:8080"
 }
